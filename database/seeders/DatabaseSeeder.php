bfff014c421be8a291e733966ac91fdccbbceeba
Avital Rubichi <avital.rubichi@gmail.com>
2026-05-19 06:18:30 +0300

    Fix UniqueConstraintViolationException on re-seeding by truncating tables first in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Prevent UniqueConstraintViolationException by truncating tables before seeding
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        Item::truncate();
+        Schema::enableForeignKeyConstraints();
 
         User::factory()->create([
             'name' => 'Test User',
